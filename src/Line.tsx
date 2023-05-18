602504d06cbe1945cbfba42ab46efb50ccd3f18b
WalkerAlfaro <alfaro.trelles@gmail.com>
2023-05-17 20:02:13 -0500

    added get measures and plot

--- src/Line.tsx
+++ src/Line.tsx
@@ -1,75 +1,29 @@
-import { ReactECharts, ReactEChartsProps } from "./ReactEcharts";
-import TEST from '../tests.json'
+import { ReactECharts } from "./ReactEcharts";
 import { useFetchMeasure } from "./hooks/useFetchMeasure";
-
-const dateList = TEST.map( ({timeStamp}) => {
-  const epoch = parseInt(timeStamp.$date.$numberLong);
-  const dateFormat = new Date(epoch);
-  const date = `${dateFormat.getUTCHours()}:${dateFormat.getUTCMinutes()}`;
-  return date
-} )
-
-const tempList =  TEST.map( ({temperature}) => {
-  return temperature
-} )
-
-
-const option: ReactEChartsProps["option"]  = {
-  visualMap: [
-    {
-      show: false,
-    }
-  ],
-  dataZoom: [
-    {
-      type: 'inside',
-      start: 0,
-      end: 100,
-    },
-    {
-      start: 0,
-      end: 100,
-    }
-  ],
-  tooltip: {
-    trigger: 'axis'
-  },
-  xAxis: [
-    {
-      data: dateList,
-      axisTick: {
-        alignWithLabel: true
-      }
-    }
-  ],
-  yAxis: [
-    {
-      max: function (value) { return value.max + 2 },
-      min: function (value) { return value.min - 2 },
-      axisTick: {
-        alignWithLabel: true
-      }
-      
-    }
-  ],
-  series: [
-    {
-      type: 'line',
-      showSymbol: false,
-      data: tempList,
-    }
-  ]
-};
+import { optionECharts } from "./helpers/optionEChart";
 
 export const Line = () => {
-
-
-  const { measures, isLoading } = useFetchMeasure('2023-03-2');
   
+  
+  const { measures, isLoading } = useFetchMeasure('2023-03-22');
+
+  const temperatures = measures.map( (measure) => {
+    return measure.temperature;
+  })
+
+  const date = measures.map( (measure) => {
+    return measure.timeStamp;
+  })
+
+  const options = optionECharts(date, temperatures)
+  
+  // console.log(measures);
+  // console.log(isLoading);
+
   
   return (
     <div>
-      <ReactECharts option={option} style={{height: "300px"}} theme="light" loading={false}/>
+      <ReactECharts option={options} style={{height: "300px"}} theme="light" loading={isLoading}/>
     </div>
   )
 }
---
+++ src/helpers/optionEChart.ts
@@ -0,0 +1,56 @@
+import { ReactEChartsProps } from "../ReactEcharts";
+
+export const optionECharts = ( dateList: string[], valueList: number[] ) => {
+
+  const option: ReactEChartsProps["option"]  = {
+    visualMap: [
+      {
+        show: false,
+      }
+    ],
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 100,
+      },
+      {
+        start: 0,
+        end: 100,
+      }
+    ],
+    tooltip: {
+      trigger: 'axis'
+    },
+    xAxis: [
+      {
+        data: dateList,
+        axisTick: {
+          alignWithLabel: true
+        }
+      }
+    ],
+    yAxis: [
+      {
+        max: function (value) { return value.max + 2 },
+        min: function (value) { return value.min - 2 },
+        axisTick: {
+          alignWithLabel: true
+        }
+        
+      }
+    ],
+    series: [
+      {
+        type: 'line',
+        showSymbol: false,
+        data: valueList,
+      }
+    ]
+  };
+
+  return (
+    option
+  )
+  
+} 
